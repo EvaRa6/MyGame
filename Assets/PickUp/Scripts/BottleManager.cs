6610e3606e000103b7d34309003d2a4f56036b7d
EvaRa6 <151725608+EvaRa6@users.noreply.github.com>
2026-03-18 03:23:36 +0500

    add win

--- Assets/PickUp/Scripts/BottleManager.cs
+++ Assets/PickUp/Scripts/BottleManager.cs
@@ -1,10 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BottleManager : MonoBehaviour
 {
     public static BottleManager instance;
     public TextMeshProUGUI bottleText;
+
+    public GameObject winPanel; 
+    public int totalBottles = 13;
 
     int bottles = 0;
 
@@ -23,9 +27,35 @@ public class BottleManager : MonoBehaviour
     public void AddBottle(int amount)
     {
         bottles += amount;
+
         if (bottleText != null)
-            bottleText.text = " " + bottles;
+            bottleText.text = bottles + " " + totalBottles;
         else
             Debug.LogWarning("Bottle Text is not assigned!");
+
+        if (bottles >= totalBottles)
+        {
+            WinGame();
+        }
     }
+
+    void WinGame()
+{
+    Debug.Log("Победа!");
+
+    if (winPanel != null)
+        winPanel.SetActive(true);
+
+    Time.timeScale = 0f;
+    Cursor.lockState = CursorLockMode.None;
+    Cursor.visible = true;
+
+    Invoke("LoadMenu", 3f);
+}
+
+void LoadMenu()
+{
+    Time.timeScale = 1f; 
+    SceneManager.LoadScene("MainMenu");
+}
 }
